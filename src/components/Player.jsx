fdd18bd3f019713741f84d9d2a3601d6fd443f28
shaibaz-02 <shaibazmd916@gmail.com>
2025-10-23 12:58:23 +0530

    added resources

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,22 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+
+  let playerName = useRef();
+
+  function handleClick() {
+    setEnteredPlayerName(playerName.current.value);
+    playerName.current.value='';
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>
+        Welcome {enteredPlayerName ? enteredPlayerName : "unknown entity"}
+      </h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
